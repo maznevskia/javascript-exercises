164462c2bcf7719c7eafdef424082d367925261b
maznevskia <andrej.maznevski13@gmail.com>
2022-05-29 08:44:19 +0200

    Complition of Exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function( string, number ) {
+    endString = '';
 
+    if ( number < 0 ) {
+        return 'ERROR';
+    }
+
+    for ( let i = 0; i < number; i++ ) {
+        endString += string;
+    }
+
+    return endString;
 };
 
 // Do not edit below this line
