cd420f4314cdad61d8a7090c4b278d5cb9e2ed21
maznevskia <andrej.maznevski13@gmail.com>
2022-05-29 09:25:08 +0200

    Complition of Exercise removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for ( let arg of args ) {
+        index = array.indexOf( arg );
 
-};
+        if ( index == -1 ) continue;
+
+        array.splice( index, 1 );
+    }
+
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
