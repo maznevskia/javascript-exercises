e079e7ae7caab17b0473c40051c90a7fef7e049d
maznevskia <andrej.maznevski13@gmail.com>
2022-06-06 16:21:46 +0200

    Complition of Exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    
+    if(!oldest.yearOfDeath)
+        oldest.yearOfDeath = new Date().getFullYear(); 
+    yearsOldest = oldest.yearOfDeath - oldest.yearOfBirth;
 
+    for ( let i = 1; i < people.length; i++) {
+        if (!people[i].yearOfDeath)
+            people[i].yearOfDeath = new Date().getFullYear(); 
+
+        let yearsCurrent = people[i].yearOfDeath - people[i].yearOfBirth
+
+        if ( yearsOldest < yearsCurrent ) {
+            yearsOldest = yearsCurrent;
+            oldest = people[i];
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
