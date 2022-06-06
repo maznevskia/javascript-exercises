25939c7d99b6781cacfc2988c9142fb771cc61a1
maznevskia <andrej.maznevski13@gmail.com>
2022-06-06 16:08:24 +0200

    Complition of Exercise getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    titles = [];
 
+    for ( i in books ) {
+        titles.push(books[i].title);
+    }
+
+    return titles;
 };
 
 // Do not edit below this line
